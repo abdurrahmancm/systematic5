8a22a0c8a83f7f91794d4a82b5c776ea7ed0b5cd
abrrahman <65624258+abrrahman@users.noreply.github.com>
2021-06-25 11:14:05 +0600

    Add files via upload

---
+++ systematic/js/main.js
@@ -0,0 +1,9 @@
+//BX SLIDER 
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+        meanMenucontainer: '#mobile_menu',
+        meanScreenwidth: 768,
+    });
+});
